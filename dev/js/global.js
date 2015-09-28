266b1ea650b40f0f0b4b5d4168e3552017b0dff8
Colin Plamondon <colinplamondon@gmail.com>
2015-09-28 21:06:15 +0300

    Move location initialization/search into Global.
    - CHG: Moved location related code into Global.js

--- dev/js/global.js
+++ dev/js/global.js
@@ -1,11 +1,108 @@
 function GlobalClass() {
   this.init = function(){
-    Global.installObservers();
+    this.installObservers();
+    if (this.hasOwnProperty('uid')) {
+      this.locationSearch();
+      this.setInitialLocation();
+      this.installAuthedObservers();
+    }
 	};
 
 	this.installObservers = function(){
-
 	};
+
+  this.installAuthedObservers = function() {
+    $('#js-city-search').click(function(){
+      $(this).select();
+    });
+  };
+
+  this.setInitialLocation = function() {
+    var user_lat = Global.currentCoords.lat;
+    var user_lon = Global.currentCoords.lon;
+
+    var geocoder = new google.maps.Geocoder();
+
+    var latlng = {lat: parseFloat(user_lat), lng: parseFloat(user_lon)};
+    geocoder.geocode({'location': latlng}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          var city = results[3].formatted_address;
+          console.log(results);
+          $('#js-city-search').val(results[3].formatted_address);
+
+        } else {
+          window.alert('Could not find the name of your Tinder location.');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+  };
+
+
+  this.locationSearch = function() {
+    var input = /** @type {!HTMLInputElement} */(
+    document.getElementById('js-city-search'));
+
+    var ac_options = {
+      "types": ["(cities)"]
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input, ac_options);
+    var infowindow = new google.maps.InfoWindow();
+
+    autocomplete.addListener('place_changed', function() {
+      $('.js-city-error').transition({'opacity':0}).hide();
+
+      infowindow.close();
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        var message = "Nada, hombre.\n\nDid you enter a city name?";
+
+        var city_error = $('.js-city-error');
+        $(city_error).transition({'opacity':0}).show();
+        $(city_error).html(message).transition({'opacity':1});
+
+
+        return;
+      }
+
+      var coord_lat = place.geometry.location.H;
+      var coord_lon = place.geometry.location.L;
+      console.log(coord_lon);
+      $.ajax({
+          type: "POST",
+          url: '/change-location',
+          data: {
+            'xAuthToken': Global.uid,
+            'new_lat': parseFloat(coord_lat).toFixed(6),
+            'new_lon': parseFloat(coord_lon).toFixed(6)
+          },
+          dataType: 'json',
+          success: function(msg) {
+            console.log(msg);
+
+            if(msg.hasOwnProperty('error')) {
+              errors = {
+                'change_timeout': 'major position change not significant'
+              };
+
+              if(msg.error == errors.change_timeout) {
+                alert("Error - Tinder only lets you change location once every 15 minutes :(");
+              }
+            } else {
+              Global.currentCoords = {
+                "lat": coord_lat,
+                "long": coord_lon
+              };
+
+              alert("Location change successful!");
+            }
+          }
+        });
+
+    });
+  };
 
 	this.getRandomInt = function(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
--- dev/js/pages/like.js
+++ dev/js/pages/like.js
@@ -5,11 +5,8 @@ function LikeClass() {
     this.originalHovercardY = $('.match-hovercard').css('top');
     this.blurEls = $(".nav, .like-ui, .rec-bar-wrap, .footer");
 
-    this.locationSearch();
     this.installObservers();
     this.installWatchers();
-
-    this.setInitialLocation();
 
     this.matchRiverStart();
     this.recommendationRiverStart();
@@ -188,95 +185,6 @@ function LikeClass() {
     }, 450);
   };
 
-  this.setInitialLocation = function() {
-    console.log(Global.currentCoords);
-    var user_lat = Global.currentCoords.lat;
-    var user_lon = Global.currentCoords.lon;
-
-    console.log(user_lat);
-    console.log(user_lon);
-    var geocoder = new google.maps.Geocoder();
-
-    var latlng = {lat: parseFloat(user_lat), lng: parseFloat(user_lon)};
-    geocoder.geocode({'location': latlng}, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        if (results[1]) {
-          var city = results[2].formatted_address;
-
-          $('#js-city-search').val(results[2].formatted_address);
-
-        } else {
-          window.alert('Could not find the name of your Tinder location.');
-        }
-      } else {
-        window.alert('Geocoder failed due to: ' + status);
-      }
-    });
-
-  };
-
-  this.locationSearch = function() {
-    var input = /** @type {!HTMLInputElement} */(
-    document.getElementById('js-city-search'));
-
-    var ac_options = {
-      "types": ["(cities)"]
-    };
-    var autocomplete = new google.maps.places.Autocomplete(input, ac_options);
-    var infowindow = new google.maps.InfoWindow();
-
-    autocomplete.addListener('place_changed', function() {
-      $('.js-city-error').transition({'opacity':0}).hide();
-
-      infowindow.close();
-      var place = autocomplete.getPlace();
-      if (!place.geometry) {
-        var message = "Nada, hombre.\n\nDid you enter a city name?";
-
-        var city_error = $('.js-city-error');
-        $(city_error).transition({'opacity':0}).show();
-        $(city_error).html(message).transition({'opacity':1});
-
-
-        return;
-      }
-
-      var coord_lat = place.geometry.location.H;
-      var coord_lon = place.geometry.location.L;
-      console.log(coord_lon);
-      $.ajax({
-          type: "POST",
-          url: '/change-location',
-          data: {
-            'xAuthToken': Global.uid,
-            'new_lat': parseFloat(coord_lat).toFixed(6),
-            'new_lon': parseFloat(coord_lon).toFixed(6)
-          },
-          dataType: 'json',
-          success: function(msg) {
-            console.log(msg);
-
-            if(msg.hasOwnProperty('error')) {
-              errors = {
-                'change_timeout': 'major position change not significant'
-              };
-
-              if(msg.error == errors.change_timeout) {
-                alert("Error - Tinder only lets you change location once every 15 minutes :(");
-              }
-            } else {
-              Global.currentCoords = {
-                "lat": coord_lat,
-                "long": coord_lon
-              };
-
-              alert("Location change successful!");
-            }
-          }
-        });
-
-    });
-  };
 
   this.matchRiverStart = function(){
     var matchRiverParams = {
